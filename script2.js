d5a325af58c5f082d30ee5962f97aa3c11cc36c5
Venezuelasystem <127757949+Venezuelasystem@users.noreply.github.com>
2023-03-28 07:56:38 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -18,13 +18,13 @@ popupWhatsApp = () => {
   let msg = document.getElementById('whats-in').value;
   let relmsg = msg.replace(/ /g,"%20");
      
-   window.open('https://wa.me/521557153381?text='+relmsg, '_blank'); 
+   window.open('https://wa.me/593998480900?text='+relmsg, '_blank'); 
   
   });
 
   setTimeout(() => {
     popup.classList.toggle('is-active-whatsapp-popup');
-  }, 3000);
+  }, 2000);
 }
 
 popupWhatsApp();
